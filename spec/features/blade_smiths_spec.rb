5cfd6b8ca739a09a6cb66e261da20f9caf0b39bf
Will Medders <wmedders@gmail.com>
2022-03-24 17:49:43 -0400

    Test: blade smiths index in order

--- spec/features/blade_smiths_spec.rb
+++ spec/features/blade_smiths_spec.rb
@@ -7,11 +7,23 @@ RSpec.describe 'blade smiths index page', type: :feature do
     blade_smith_3 = BladeSmith.create!(name: "Randall Made Knives", state: "FL", avg_yearly_output: 2043, hand_forger: true)
     blade_smith_4 = BladeSmith.create!(name: "Ka-Bar Knives, Inc.", state: "NY", avg_yearly_output: 15465, hand_forger: false)
     visit '/blade_smiths'
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content(blade_smith_1.name)
     expect(page).to have_content(blade_smith_2.name)
     expect(page).to have_content(blade_smith_3.name)
     expect(page).to have_content(blade_smith_4.name)
+  end
+
+  it 'displays blade smiths in order most recently created' do
+    blade_smith_1 = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
+    blade_smith_2 = BladeSmith.create!(name: "Bill Scagel", state: "MI", avg_yearly_output: 104, hand_forger: true)
+    blade_smith_3 = BladeSmith.create!(name: "Randall Made Knives", state: "FL", avg_yearly_output: 2043, hand_forger: true)
+    blade_smith_4 = BladeSmith.create!(name: "Ka-Bar Knives, Inc.", state: "NY", avg_yearly_output: 15465, hand_forger: false)
+    visit '/blade_smiths'
+
+    expect(blade_smith_4.name).to appear_before(blade_smith_3.name)
+    expect(blade_smith_3.name).to appear_before(blade_smith_2.name)
+    expect(blade_smith_2.name).to appear_before(blade_smith_1.name)    
   end
 end
 
@@ -22,7 +34,7 @@ RSpec.describe 'blade smiths show page', type: :feature do
     blade_smith_3 = BladeSmith.create!(name: "Randall Made Knives", state: "FL", avg_yearly_output: 2043, hand_forger: true)
     blade_smith_4 = BladeSmith.create!(name: "Ka-Bar Knives, Inc.", state: "NY", avg_yearly_output: 15465, hand_forger: false)
     visit "/blade_smiths/#{blade_smith_1.id}"
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content(blade_smith_1.name)
     expect(page).to have_content(blade_smith_1.state)
     expect(page).to have_content(blade_smith_1.avg_yearly_output)
