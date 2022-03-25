85a0e8255578321dc06e73dc36b18dda183b6c1f
Will Medders <wmedders@gmail.com>
2022-03-24 21:24:02 -0400

    Test: link to blades index inside blades index

--- spec/features/blades_spec.rb
+++ spec/features/blades_spec.rb
@@ -1,70 +1,72 @@
 require 'rails_helper'
 
-RSpec.describe 'blades index page', type: :feature do
-  it 'displays the model of each blade and attributes' do
-    bs = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
-    blade_1 = bs.blades.create!(model: "Patch Knife", length: 135, blade_material: "Damascus", handle_material: "Walnut", available: true)
-    blade_2 = bs.blades.create!(model: "Diving Knife", length: 190, blade_material: "Stainless", handle_material: "Micarta", available: true)
-    blade_3 = bs.blades.create!(model: "Fighting Knife", length: 240, blade_material: "Tool Steel", handle_material: "Stacked Leather", available: false)
-    blade_4 = bs.blades.create!(model: "Skinner", length: 170, blade_material: "High Carbon Steel", handle_material: "Figured Maple", available: false)
-
-    visit '/blades'
-    save_and_open_page
-    expect(page).to have_content(blade_1.model)
-    expect(page).to have_content(blade_1.length)
-    expect(page).to have_content(blade_1.blade_material)
-    expect(page).to have_content(blade_1.handle_material)
-    expect(page).to have_content(blade_1.available)
-    expect(page).to have_content(blade_1.id)
-    expect(page).to have_content(blade_1.created_at)
-    expect(page).to have_content(blade_1.updated_at)
-
-    expect(page).to have_content(blade_2.model)
-    expect(page).to have_content(blade_2.length)
-    expect(page).to have_content(blade_2.blade_material)
-    expect(page).to have_content(blade_2.handle_material)
-    expect(page).to have_content(blade_2.available)
-    expect(page).to have_content(blade_2.id)
-    expect(page).to have_content(blade_2.created_at)
-    expect(page).to have_content(blade_2.updated_at)
-
-    expect(page).to have_content(blade_3.model)
-    expect(page).to have_content(blade_3.length)
-    expect(page).to have_content(blade_3.blade_material)
-    expect(page).to have_content(blade_3.handle_material)
-    expect(page).to have_content(blade_3.available)
-    expect(page).to have_content(blade_3.id)
-    expect(page).to have_content(blade_3.created_at)
-    expect(page).to have_content(blade_3.updated_at)
-
-    expect(page).to have_content(blade_4.model)
-    expect(page).to have_content(blade_4.length)
-    expect(page).to have_content(blade_4.blade_material)
-    expect(page).to have_content(blade_4.handle_material)
-    expect(page).to have_content(blade_4.available)
-    expect(page).to have_content(blade_4.id)
-    expect(page).to have_content(blade_4.created_at)
-    expect(page).to have_content(blade_4.updated_at)
+RSpec.describe 'blades' do
+  before :each do
+    @bs = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
+    @blade_1 = @bs.blades.create!(model: "Patch Knife", length: 135, blade_material: "Damascus", handle_material: "Walnut", available: true)
+    @blade_2 = @bs.blades.create!(model: "Diving Knife", length: 190, blade_material: "Stainless", handle_material: "Micarta", available: true)
+    @blade_3 = @bs.blades.create!(model: "Fighting Knife", length: 240, blade_material: "Tool Steel", handle_material: "Stacked Leather", available: false)
+    @blade_4 = @bs.blades.create!(model: "Skinner", length: 170, blade_material: "High Carbon Steel", handle_material: "Figured Maple", available: false)
   end
-end
+  describe 'blades index page', type: :feature do
+    it 'displays the model of each blade and attributes' do
+      visit '/blades'
+      expect(page).to have_content(@blade_1.model)
+      expect(page).to have_content(@blade_1.length)
+      expect(page).to have_content(@blade_1.blade_material)
+      expect(page).to have_content(@blade_1.handle_material)
+      expect(page).to have_content(@blade_1.available)
+      expect(page).to have_content(@blade_1.id)
+      expect(page).to have_content(@blade_1.created_at)
+      expect(page).to have_content(@blade_1.updated_at)
 
-RSpec.describe 'blades show page', type: :feature do
-  it 'displays the model of blade with corresponding id and attributes' do
-    bs = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
-    blade_1 = bs.blades.create!(model: "Patch Knife", length: 135, blade_material: "Damascus", handle_material: "Walnut", available: true)
-    blade_2 = bs.blades.create!(model: "Diving Knife", length: 190, blade_material: "Stainless", handle_material: "Micarta", available: true)
-    blade_3 = bs.blades.create!(model: "Fighting Knife", length: 240, blade_material: "Tool Steel", handle_material: "Stacked Leather", available: false)
-    blade_4 = bs.blades.create!(model: "Skinner", length: 170, blade_material: "High Carbon Steel", handle_material: "Figured Maple", available: false)
+      expect(page).to have_content(@blade_2.model)
+      expect(page).to have_content(@blade_2.length)
+      expect(page).to have_content(@blade_2.blade_material)
+      expect(page).to have_content(@blade_2.handle_material)
+      expect(page).to have_content(@blade_2.available)
+      expect(page).to have_content(@blade_2.id)
+      expect(page).to have_content(@blade_2.created_at)
+      expect(page).to have_content(@blade_2.updated_at)
 
-    visit "/blades/#{blade_1.id}"
-    save_and_open_page
-    expect(page).to have_content(blade_1.model)
-    expect(page).to have_content(blade_1.length)
-    expect(page).to have_content(blade_1.blade_material)
-    expect(page).to have_content(blade_1.handle_material)
-    expect(page).to have_content(blade_1.available)
-    expect(page).to have_content(blade_1.id)
-    expect(page).to have_content(blade_1.created_at)
-    expect(page).to have_content(blade_1.updated_at)
+      expect(page).to have_content(@blade_3.model)
+      expect(page).to have_content(@blade_3.length)
+      expect(page).to have_content(@blade_3.blade_material)
+      expect(page).to have_content(@blade_3.handle_material)
+      expect(page).to have_content(@blade_3.available)
+      expect(page).to have_content(@blade_3.id)
+      expect(page).to have_content(@blade_3.created_at)
+      expect(page).to have_content(@blade_3.updated_at)
+
+      expect(page).to have_content(@blade_4.model)
+      expect(page).to have_content(@blade_4.length)
+      expect(page).to have_content(@blade_4.blade_material)
+      expect(page).to have_content(@blade_4.handle_material)
+      expect(page).to have_content(@blade_4.available)
+      expect(page).to have_content(@blade_4.id)
+      expect(page).to have_content(@blade_4.created_at)
+      expect(page).to have_content(@blade_4.updated_at)
+    end
+
+    it 'has a link to the Blades index at the top' do
+      visit "/blades"
+      save_and_open_page
+      expect(page).to have_link(nil, href: '/blades')
+    end
+  end
+
+
+  describe 'blades show page', type: :feature do
+    it 'displays the model of blade with corresponding id and attributes' do
+      visit "/blades/#{@blade_1.id}"
+      expect(page).to have_content(@blade_1.model)
+      expect(page).to have_content(@blade_1.length)
+      expect(page).to have_content(@blade_1.blade_material)
+      expect(page).to have_content(@blade_1.handle_material)
+      expect(page).to have_content(@blade_1.available)
+      expect(page).to have_content(@blade_1.id)
+      expect(page).to have_content(@blade_1.created_at)
+      expect(page).to have_content(@blade_1.updated_at)
+    end
   end
 end
