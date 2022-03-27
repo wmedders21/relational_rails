a8e1674bba66748993a33ac0f66c6a707817f265
Will Medders <wmedders@gmail.com>
2022-03-27 17:54:16 -0400

    Test: update page exists

--- spec/features/blade_smiths/new_spec.rb
+++ spec/features/blade_smiths/new_spec.rb
@@ -18,30 +18,4 @@ RSpec.describe 'blade smiths new page', type: :feature do
     expect(current_path).to eq("/blade_smiths/#{new_blade_smith_id}")
     expect(page).to have_content("Cold Steel")
   end
-  # xit 'displays blade smiths in order most recently created' do
-  #   blade_smith_1 = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
-  #   blade_smith_2 = BladeSmith.create!(name: "Bill Scagel", state: "MI", avg_yearly_output: 104, hand_forger: true)
-  #   blade_smith_3 = BladeSmith.create!(name: "Randall Made Knives", state: "FL", avg_yearly_output: 2043, hand_forger: true)
-  #   blade_smith_4 = BladeSmith.create!(name: "Ka-Bar Knives, Inc.", state: "NY", avg_yearly_output: 15465, hand_forger: false)
-  #   visit '/blade_smiths'
-  #   expect(blade_smith_4.name).to appear_before(blade_smith_3.name)
-  #   expect(blade_smith_3.name).to appear_before(blade_smith_2.name)
-  #   expect(blade_smith_2.name).to appear_before(blade_smith_1.name)
-  # end
-  #
-  # xit 'has a link to the Blades index at the top' do
-  #   blade_smith_1 = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
-  #   visit "/blade_smiths"
-  #   expect(page).to have_link(nil, href: '/blades')
-  # end
-  #
-  # xit 'has a link to the BladeSmiths index at the top' do
-  #   visit "/blade_smiths"
-  #   expect(page).to have_link(nil, href: '/blade_smiths')
-  # end
-  #
-  # xit 'has a link to BladeSmiths new' do
-  #   visit "/blade_smiths"
-  #   expect(page).to have_link(nil, href: '/blade_smiths/new')
-  # end
 end
---
+++ spec/features/blade_smiths/update_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'blade smiths update page', type: :feature do
+  before :each do
+    @blade_smith = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
+  end
+  it 'exists' do
+    visit "/blade_smiths/#{@blade_smith.id}/edit"
+    expect(current_path).to eq("/blade_smiths/#{@blade_smith.id}/edit")
+  end
+
+  xit 'can update an existing bladesmith' do
+    visit "/blade_smiths/#{@blade_smith.id}/edit"
+    fill_in('Avg yearly output', with: 13009)
+    click_button('Create Bladesmith')
+
+    expect(current_path).to eq("/blade_smiths/#{@blade_smith.id}")
+    expect(page).to have_content(13009)
+  end
+end
