b393aca73e689e8e6841e159c6beccb01c08b34b
Will Medders <wmedders@gmail.com>
2022-03-28 21:39:50 -0400

    Test: blade model #sorted_by_model

--- spec/models/blade_spec.rb
+++ spec/models/blade_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Blade, type: :model do
   it {should belong_to :blade_smith}
 
-  describe '#available?' do
+  describe 'class methods' do
     before :each do
       @blade_smith_1 = BladeSmith.create!(name: "Willy White", state: "KY", avg_yearly_output: 324, hand_forger: true)
       @blade_1 = @blade_smith_1.blades.create!(model: "Patch Knife", length: 135, blade_material: "Damascus", handle_material: "Walnut", available: true)
@@ -11,8 +11,16 @@ RSpec.describe Blade, type: :model do
       @blade_3 = @blade_smith_1.blades.create!(model: "Fighting Knife", length: 240, blade_material: "Tool Steel", handle_material: "Stacked Leather", available: false)
       @blade_4 = @blade_smith_1.blades.create!(model: "Skinner", length: 170, blade_material: "High Carbon Steel", handle_material: "Figured Maple", available: false)
     end
-    it 'returns blades where the attribute available == true' do
-      expect(@blade_smith_1.blades.available?).to eq([@blade_1, @blade_2])
+    describe '#available' do
+      it 'returns blades where the attribute available == true' do
+        expect(@blade_smith_1.blades.available?).to eq([@blade_1, @blade_2])
+      end
+    end
+
+    describe '#sorted_by_model' do
+      it 'returns blades sorted alphabetically by model' do
+        expect(@blade_smith_1.blades.sorted_by_model).to eq([@blade_2, @blade_3, @blade_1, @blade_4])
+      end
     end
   end
 end
