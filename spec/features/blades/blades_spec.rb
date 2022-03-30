ac67a7b7e78034e4f7d418e0cf377c3d25378d37
Will Medders <wmedders@gmail.com>
2022-03-29 21:03:35 -0400

    Test: each blade has link to delete blade on blade index

--- spec/features/blades/blades_spec.rb
+++ spec/features/blades/blades_spec.rb
@@ -58,6 +58,18 @@ RSpec.describe 'blades' do
       click_link("Update #{@blade_1.model}")
       expect(current_path).to eq("/blades/#{@blade_1.id}/edit")
     end
+
+    it 'has links to delete each blade' do
+      visit "/blades"
+      expect(page).to have_content(@blade_1.model)
+      expect(page).to have_content(@blade_2.model)
+
+      click_link("Delete #{@blade_1.model}")
+      expect(current_path).to eq("/blades")
+      expect(page).to have_no_content(@blade_1.model)
+      click_link("Delete #{@blade_2.model}")
+      expect(page).to have_no_content(@blade_2.model)
+    end
   end
 
   describe 'blades show page', type: :feature do
