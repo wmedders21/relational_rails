b4a6d62807617ed8b6b390c9f307c4ad275a38b7
Will Medders <wmedders@gmail.com>
2022-03-29 20:47:10 -0400

    Test: bladesmiths index has delete link for each bladesmith

--- spec/features/blade_smiths/blade_smiths_spec.rb
+++ spec/features/blade_smiths/blade_smiths_spec.rb
@@ -48,6 +48,23 @@ RSpec.describe 'blade smiths index page', type: :feature do
     click_link("Update #{@blade_smith_1.name}")
     expect(current_path).to eq("/blade_smiths/#{@blade_smith_1.id}/edit")
   end
+
+  it 'has links to delete each bladesmith' do
+    visit "/blade_smiths"
+    expect(page).to have_content("#{@blade_smith_1.name}")
+    expect(page).to have_content("#{@blade_smith_2.name}")
+    expect(page).to have_content("#{@blade_smith_3.name}")
+    expect(page).to have_content("#{@blade_smith_4.name}")
+    click_link("Delete #{@blade_smith_1}")
+    expect(current_path).to eq('/blade_smiths')
+    expect(page).to have_no_content("#{@blade_smith_1.name}")
+    click_link("Delete #{@blade_smith_2}")
+    expect(page).to have_no_content("#{@blade_smith_2.name}")
+    click_link("Delete #{@blade_smith_3}")
+    expect(page).to have_no_content("#{@blade_smith_3.name}")
+    click_link("Delete #{@blade_smith_4}")
+    expect(page).to have_no_content("#{@blade_smith_4.name}")
+  end
 end
 
 RSpec.describe 'blade smiths show page', type: :feature do
